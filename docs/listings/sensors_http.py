2f04a6bb02ddb1c79f765593d99ac2f85c0dcd91
mada1019 <daniel-j-mayer@gmx.de>
2016-03-26 20:53:32 +0100

    anlage hardware done sftware 2go

--- docs/listings/sensors_http.py
+++ docs/listings/sensors_http.py
@@ -1,99 +1,41 @@
 from httplib import HTTPConnection
 
-import logging
-logging.basicConfig()
-log = logging.getLogger()
-log.setLevel(logging.INFO)
-
 class SensorsHttp():
-
     @property
     def server_address(self):
-
         return self.server.host
 
     @property
     def server_port(self):
-
         return str(self.server.port)
 
-
     def __init__(self, server_address, server_port):
-
-        self.server = HTTPConnection( \
-            host = server_address, port = int(server_port))
-
+        self.server = HTTPConnection(host = server_address, port = int(server_port))
 
     def __connect_to_server(self):
-
-        try:
-            logging.debug("Attempting to connect to server " + \
-                self.server_address + ":" + self.server_port)
-
-            self.server.connect()
-
-            logging.debug("Successfully connected to server " + \
-                self.server_address + ":" + self.server_port)
-
-        except:
-            logging.error("Failed to connect to server " + \
-                self.server_address + ":" + self.server_port)
-
+        self.server.connect()
 
     def __disconnect_from_server(self):
-
-        try:
-            logging.debug("Attempting to disconnect from server " + \
-                self.server_address + ":" + self.server_port)
-
-            self.server.close()
-            
-            logging.debug("Successfully disconnected from server " + \
-                self.server_address + ":" + self.server_port)
-
-        except:
-            logging.error("Failed to disconnect from server " + \
-                self.server_address + ":" + self.server_port)
-
+        self.server.close()
 
     def __get_wt(self, unit):
-
         self.__connect_to_server()
-
-        try:
-            logging.debug("Attempting to read from WT unit " + str(unit))
-            self.server.request('GET', "/Single" + str(unit))
-
-            response_string = self.server.getresponse().read()
-            logging.debug("Successfully read from WT unit " + str(unit))
-
-            temperature = \
-                float(response_string.split(";")[-1][:4].replace(",", "."))
-            return temperature
-
-        except:
-            logging.error("Failed to read from WT unit " + str(unit))
-
+        self.server.request('GET', "/Single" + str(unit))
+        response_string = self.server.getresponse().read()
+        temperature = float(response_string.split(";")[-1][:4].replace(",", "."))
+        return temperature
         finally:
             self.__disconnect_from_server()
 
-
     def get_wt_0(self):
-
         return self.__get_wt(unit = 1)
 
-
     def get_wt_1(self):
-
         return self.__get_wt(unit = 2)
 
-
     def get_wt_2(self):
-
         return self.__get_wt(unit = 3)
 
-
     def get_wt_3(self):
-
         return self.__get_wt(unit = 4)
         
